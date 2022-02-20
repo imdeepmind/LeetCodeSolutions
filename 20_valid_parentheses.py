7483693ec99bcad22978fe73749ad68387c4002b
imdeepmind <abhishekchatterjeejit@gmail.com>
2022-02-20 22:08:13 +0530

    added 20 valid paranthesis

---
+++ 20_valid_parentheses.py
@@ -0,0 +1,30 @@
+class Solution:
+    def isValid(self, s: str) -> bool:
+        start_stack = []
+        end_stack = []
+        opp = {
+            '(': ')',
+            ')': '(',
+            '{': '}',
+            '}': '{',
+            '[': ']',
+            ']': '['
+        }
+        
+        
+        for p in s:
+            if p in ('(', '{', '['):
+                start_stack.append(p)
+            else:
+                if len(start_stack) > 0 and start_stack[-1] == opp[p]:
+                    del start_stack[-1]
+                else:
+                    end_stack.append(p)
+        
+        return True if len(start_stack) == 0 and len(end_stack) == 0 else False
+
+            
+
+    
+sol = Solution()
+print(sol.isValid("(()){}[]"))
