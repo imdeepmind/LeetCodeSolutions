17cd65a721a6b9c80af04a33da3d071b1a201788
Abhishek Chatterjee <abhishek.chatterjee97@protonmail.com>
2022-05-20 00:14:31 +0530

    Time: 98 ms (53.59%), Space: 15.2 MB (24.34%) - LeetHub

---
+++ 1-two-sum/1-two-sum.py
@@ -0,0 +1,11 @@
+class Solution:
+    def twoSum(self, nums: List[int], target: int) -> List[int]:
+        hash_num = {}
+        
+        for index, num in enumerate(nums):
+            if not num in hash_num:
+                hash_num[num] = index
+            
+            t = target - num
+            if t in hash_num and hash_num[t] != index:
+                return hash_num[t], index
