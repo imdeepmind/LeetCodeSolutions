1a53765cf8315a9c4fb515afe1dd31e232f941ee
Abhishek Chatterjee <abhishek.chatterjee97@protonmail.com>
2022-05-20 23:04:48 +0530

    Update 2-add-two-numbers.py

--- 2-add-two-numbers/2-add-two-numbers.py
+++ 2-add-two-numbers/2-add-two-numbers.py
@@ -4,25 +4,29 @@
 #         self.val = val
 #         self.next = next
 
+
 class Solution:
     def addTwoNumbers(self, l1: Optional[ListNode], l2: Optional[ListNode]) -> Optional[ListNode]:
-        dummy = ListNode(0)
-        temp = dummy
+        start = ListNode()
+        curr = start
+        
         carry = 0
         
-        while l1 != None or l2 != None or carry:
-            sum1 = 0
-            if l1 != None:
-                sum1 += l1.val
-                l1 = l1.next
+        while l1 or l2 or carry:
+            num1 = l1.val if l1 else 0
+            num2 = l2.val if l2 else 0
             
-            if l2 != None:
-                sum1 += l2.val
-                l2 = l2.next
-                
-            sum1 += carry
-            carry = sum1//10
-            node = ListNode(sum1%10)
-            temp.next = node
-            temp = temp.next
-        return dummy.next
+            s = num1 + num2 + carry
+            
+            if s >= 10:
+                carry = s // 10
+                s = s % 10
+            
+            curr.next = ListNode(s)
+            curr = curr.next
+            
+            l1 = l1.next if l1 else None
+            l2 = l2.next if l2 else None
+            
+        
+        return start.next
