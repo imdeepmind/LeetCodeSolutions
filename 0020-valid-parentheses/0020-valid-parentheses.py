29af89133c551ce546c75d8c86a09f0f7f2312d9
Abhishek Chatterjee <abhishek@imdeepmind.com>
2023-07-19 22:55:40 +0530

    Time: 47 ms (61.61%), Space: 16.2 MB (88.98%) - LeetHub

--- 0020-valid-parentheses/0020-valid-parentheses.py
+++ 0020-valid-parentheses/0020-valid-parentheses.py
@@ -24,27 +24,3 @@ class Solution:
             
         return len(start_bracket) == 0 and len(end_bracket) == 0
     
-    def isValidOld(self, s:str) -> bool:
-        start_bracket = []
-        end_bracket = []
-        
-        start_bracket_sign = "({["
-        opp = {
-            '(': ')',
-            ')': '(',
-            '{': '}',
-            '}': '{',
-            '[': ']',
-            ']': '['
-        }
-        
-        for c in s:
-            if c in start_bracket_sign:
-                start_bracket.append(c)
-            else:
-                if len(start_bracket) > 0 and start_bracket[-1] == opp[c]:
-                    del start_bracket[-1]
-                else:
-                    end_bracket.append(c)
-            
-        return len(start_bracket) == 0 and len(end_bracket) == 0
