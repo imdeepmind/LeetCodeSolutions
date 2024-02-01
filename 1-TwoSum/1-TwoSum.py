a80a5f070760c25e788bf44245bf9287323a631d
Abhishek Chatterjee <abhishek@imdeepmind.com>
2024-02-01 20:50:17 +0530

    Create file

---
+++ 1-TwoSum/1-TwoSum.py
@@ -0,0 +1,16 @@
+class Solution:
+    def twoSum(self, nums: List[int], target: int) -> List[int]:
+        hash = {}
+        
+        for index, num in enumerate(nums):
+            hash[num] = index
+        
+        for index, num in enumerate(nums):
+            alpha = target - num
+            
+            if alpha in hash and hash[alpha] != index:
+                return [index, hash[alpha]]
+        
+        return None
+            
+[2,7,11,15]
