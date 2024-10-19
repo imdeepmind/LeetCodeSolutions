48bdca2fbe53218867d74fdc309fb16d49795dee
Abhishek Chatterjee <abhishek@imdeepmind.com>
2024-10-19 22:20:30 +0530

    Time: 183 ms (75.57%), Space: 0B (100%) - LeetHub

---
+++ 0610-triangle-judgement/0610-triangle-judgement.sql
@@ -0,0 +1,7 @@
+-- Write your PostgreSQL query statement below
+SELECT *, 
+    CASE 
+        WHEN x+y > z AND x+z > y AND y+z > x THEN 'Yes' 
+        ELSE 'No'
+    END AS triangle
+FROM Triangle
