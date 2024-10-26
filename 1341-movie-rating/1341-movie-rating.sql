5e1182842510a8015acd0514203117bea1cafb1d
Abhishek Chatterjee <abhishek@imdeepmind.com>
2024-10-27 01:14:03 +0530

    Time: 339 ms (27.22%), Space: 0B (100%) - LeetHub

--- 1341-movie-rating/1341-movie-rating.sql
+++ 1341-movie-rating/1341-movie-rating.sql
@@ -1,24 +1,22 @@
 -- Write your PostgreSQL query statement below
-SELECT DISTINCT results FROM (
-    (
-        SELECT U.name results
-        FROM MovieRating MR
-        INNER JOIN Users U
-            ON U.user_id = MR.user_id
-        GROUP BY MR.user_id, U.name
-        ORDER BY COUNT(MR.user_id) DESC, name ASC
-        LIMIT 1
-    )
-    UNION
-    (
-        SELECT
-            M.title AS results
-        FROM MovieRating MR
-        INNER JOIN Movies M
-            ON M.movie_id = MR.movie_id
-        WHERE EXTRACT(MONTH FROM MR.created_at) = 2 AND EXTRACT(YEAR FROM MR.created_at) = 2020
-        GROUP BY MR.movie_id, M.title
-        ORDER BY AVG(MR.rating) DESC, M.title
-        LIMIT 1
-    )
+(
+    SELECT U.name results
+    FROM MovieRating MR
+    INNER JOIN Users U
+        ON U.user_id = MR.user_id
+    GROUP BY MR.user_id, U.name
+    ORDER BY COUNT(MR.user_id) DESC, name ASC
+    LIMIT 1
+)
+UNION ALL
+(
+    SELECT
+        M.title AS results
+    FROM MovieRating MR
+    INNER JOIN Movies M
+        ON M.movie_id = MR.movie_id
+    WHERE EXTRACT(MONTH FROM MR.created_at) = 2 AND EXTRACT(YEAR FROM MR.created_at) = 2020
+    GROUP BY MR.movie_id, M.title
+    ORDER BY AVG(MR.rating) DESC, M.title
+    LIMIT 1
 )
