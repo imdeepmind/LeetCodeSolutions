bb9a7c53533af087e8584f2fbc12eff1d23b7989
Abhishek Chatterjee <abhishek@imdeepmind.com>
2024-10-26 22:44:15 +0530

    Time: 308 ms (75.64%), Space: 0B (100%) - LeetHub

--- 0570-managers-with-at-least-5-direct-reports/0570-managers-with-at-least-5-direct-reports.sql
+++ 0570-managers-with-at-least-5-direct-reports/0570-managers-with-at-least-5-direct-reports.sql
@@ -1,7 +1,17 @@
 -- Write your PostgreSQL query statement below
-SELECT 
-    E.name
+-- SELECT 
+--     E.name
+-- FROM Employee E
+-- WHERE (
+--     SELECT COUNT(*) FROM Employee E2 WHERE E2.managerId = E.id
+-- ) >= 5
+
+WITH tmp_table AS (
+    SELECT managerId
+    FROM Employee 
+    GROUP BY managerId
+    HAVING COUNT(managerId) >= 5
+)
+SELECT name
 FROM Employee E
-WHERE (
-    SELECT COUNT(*) FROM Employee E2 WHERE E2.managerId = E.id
-) >= 5
+WHERE E.id IN (SELECT DISTINCT managerId FROM tmp_table)
