440a8a7afca9e06dd77b802a2dbeb6cda8874045
Abhishek Chatterjee <abhishek@imdeepmind.com>
2024-10-27 23:19:14 +0530

    Time: 179 ms (86.49%), Space: 0B (100%) - LeetHub

--- 3220-odd-and-even-transactions/3220-odd-and-even-transactions.sql
+++ 3220-odd-and-even-transactions/3220-odd-and-even-transactions.sql
@@ -1,25 +1,8 @@
 -- -- Write your PostgreSQL query statement below
-WITH tmp_table AS (
-    SELECT transaction_date, SUM(amount) AS amount, amount % 2 = 0 AS is_even
-    FROM transactions
-    GROUP BY amount % 2 = 0, transaction_date
-    ORDER BY transaction_date ASC
-)
 SELECT 
     transaction_date,
-    SUM(odd_sum) AS odd_sum,
-    SUM(even_sum) AS even_sum
-FROM (
-    SELECT 
-        transaction_date,
-        CASE 
-            WHEN is_even = FALSE THEN amount
-            ELSE 0
-        END AS odd_sum,
-        CASE 
-            WHEN is_even = TRUE THEN amount
-            ELSE 0
-        END AS even_sum
-    FROM tmp_table
-)
+    SUM(CASE WHEN amount % 2 != 0 THEN amount ELSE 0 END) AS odd_sum,
+    SUM(CASE WHEN amount % 2 = 0 THEN amount ELSE 0 END) AS even_sum
+FROM transactions
 GROUP BY transaction_date
+ORDER BY transaction_date ASC
