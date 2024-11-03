60937365c76c7b29a12cf769445e169e25c40247
Abhishek Chatterjee <abhishek@imdeepmind.com>
2024-11-03 13:13:30 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

---
+++ 2704-to-be-or-not-to-be/2704-to-be-or-not-to-be.js
@@ -0,0 +1,16 @@
+/**
+ * @param {string} val
+ * @return {Object}
+ */
+var expect = function(val) {
+    return {
+        toBe: compare => {
+            if (compare == val) return true
+            else throw new Error("Not Equal")
+        },
+        notToBe: compare => {
+            if (compare !== val) return true
+            else throw new Error("Equal")
+        }
+    }
+};
