055b48d354416be9b83f8d29e9fb639cb4d8b3f4
Abhishek Chatterjee <abhishek@imdeepmind.com>
2024-11-09 17:31:28 +0530

    Time: 1 ms (61.73%), Space: 17.8 MB (51.19%) - LeetHub

--- 0001-two-sum/0001-two-sum.py
+++ 0001-two-sum/0001-two-sum.py
@@ -1,15 +1,14 @@
 class Solution:
     def twoSum(self, nums: List[int], target: int) -> List[int]:
         hash = {}
-        
+
         for index, num in enumerate(nums):
             hash[num] = index
         
         for index, num in enumerate(nums):
             alpha = target - num
-            
+
             if alpha in hash and hash[alpha] != index:
-                return [index, hash[alpha]]
+                return index, hash[alpha]
         
-        return None
-            
+        return 0, 0
