59ec1be42b2567402037fa801d40f0ffeaa968da
Abhishek Chatterjee <abhishek@imdeepmind.com>
2024-11-12 22:25:35 +0530

    Time: 3 ms (98.84%), Space: 18.4 MB (18.14%) - LeetHub

---
+++ 0125-valid-palindrome/0125-valid-palindrome-better.py
@@ -0,0 +1,20 @@
+import re
+
+class Solution:
+    def isPalindrome(self, s: str) -> bool:
+        s = s.lower()
+        s = s.replace(" ", "")
+        s = re.sub('[^0-9a-zA-Z]+', '', s)
+
+        if s == "":
+            return True
+
+        for i in range(len(s)//2+1):
+            alpha = len(s) - (i+1)
+
+            if s[i] != s[alpha]:
+                return False
+        
+        return True
+
+        # return s == s[::-1]
