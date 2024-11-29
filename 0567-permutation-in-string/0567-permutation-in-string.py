4706d5a32bfddf366b178091b0adab14cc390c5c
Abhishek Chatterjee <abhishek@imdeepmind.com>
2024-11-29 20:01:16 +0530

    Time: 1072 ms (18.48%), Space: 17.3 MB (10.5%) - LeetHub

--- 0567-permutation-in-string/0567-permutation-in-string.py
+++ 0567-permutation-in-string/0567-permutation-in-string.py
@@ -5,10 +5,13 @@ class Solution:
         if len(s1) > len(s2): return False
         if s1 == s2: return True
 
+        n1 = len(s1)
+        n2 = len(s2)
+
         s1Freq = Counter(s1)
 
-        for i in range(len(s2)-len(s1)+1):
-            if Counter(s2[i:i+len(s1)]) == s1Freq:
+        for i in range(n2-n1+1):
+            if Counter(s2[i:i+n1]) == s1Freq:
                 return True
         
         return False
