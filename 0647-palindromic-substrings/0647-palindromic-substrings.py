b1b93312b8b561a1989717cc3b337a31bd5d60b4
Abhishek Chatterjee <abhishek@imdeepmind.com>
2024-12-03 22:44:55 +0530

    Time: 104 ms (88.84%), Space: 17.1 MB (30.2%) - LeetHub

--- 0647-palindromic-substrings/0647-palindromic-substrings.py
+++ 0647-palindromic-substrings/0647-palindromic-substrings.py
@@ -5,13 +5,13 @@ class Solution:
         if n == 1:
             return 1
 
-        res = []
+        res = 0
 
         for i in range(n):
             l, r = i, i
 
             while l >= 0 and n > r and  s[l] == s[r]:
-                res.append(s[l:r+1])
+                res += 1
                 
                 l -= 1
                 r += 1
@@ -19,10 +19,10 @@ class Solution:
             l, r = i, i+1
 
             while l >= 0 and n > r and  s[l] == s[r]:
-                res.append(s[l:r+1])
+                res += 1
 
                 l -= 1
                 r += 1
 
-        return len(res)
+        return res
 
