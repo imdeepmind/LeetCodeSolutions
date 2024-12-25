e343d5339bf0dbb2ec1ab1f0ef98051a72aa9f6a
Abhishek Chatterjee <abhishek@imdeepmind.com>
2024-12-25 15:07:10 +0530

    Time: 4 ms (69.44%), Space: 18.1 MB (5.94%) - LeetHub

---
+++ 0415-add-strings/0415-add-strings-two-pointer-faster.py
@@ -0,0 +1,33 @@
+class Solution:
+    def addStrings(self, num1: str, num2: str) -> str:
+        if num1 == "0":
+            return num2
+        
+        if num2 == "0":
+            return num1
+        
+        i, j = len(num1) - 1, len(num2) - 1
+
+        res = ""
+        carry = 0
+
+        while i >= 0 or j >= 0:
+            x = int(num1[i]) if i >= 0 else 0
+            y = int(num2[j]) if j >= 0 else 0
+
+            alpha = x + y + carry
+            carry = 0
+
+            if alpha > 9:
+                carry = 1
+                alpha = alpha % 10
+            
+            res = str(alpha) + res
+
+            i -= 1
+            j -= 1
+
+        if carry == 1:
+            res = "1" + res
+
+        return res
