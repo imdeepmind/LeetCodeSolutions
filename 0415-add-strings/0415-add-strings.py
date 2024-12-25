8646b32840471778025ca1b9107a1ddc6646481b
Abhishek Chatterjee <abhishek@imdeepmind.com>
2024-12-25 15:06:50 +0530

    Time: 7 ms (49.44%), Space: 17.8 MB (14.77%) - LeetHub

--- 0415-add-strings/0415-add-strings.py
+++ 0415-add-strings/0415-add-strings.py
@@ -6,32 +6,16 @@ class Solution:
         if num2 == "0":
             return num1
         
-        n1 = len(num1)
-        n2 = len(num2)
-
-        # Find the big number in length
-        big = ""
-        small = ""
-        if n1 > n2:
-            big = num1
-            small = num2
-        else:
-            big = num2
-            small = num1
-        
-        # Iterate through the big
-        big = big[::-1]
-        small = small[::-1]
+        i, j = len(num1) - 1, len(num2) - 1
 
         res = ""
         carry = 0
 
-        for index, b_num in enumerate(big):
-            s_num = "0"
-            if len(small) > index:
-                s_num = small[index]
-            
-            alpha = int(s_num) + int(b_num) + carry
+        while i >= 0 or j >= 0:
+            x = int(num1[i]) if i >= 0 else 0
+            y = int(num2[j]) if j >= 0 else 0
+
+            alpha = x + y + carry
             carry = 0
 
             if alpha > 9:
@@ -40,7 +24,10 @@ class Solution:
             
             res += str(alpha)
 
+            i -= 1
+            j -= 1
+
         if carry == 1:
             res += "1"
-            
+
         return res[::-1]
