f30d5cb310116fce2107be66eca8bec3e45fc064
Abhishek Chatterjee <abhishek@imdeepmind.com>
2024-12-25 22:49:11 +0530

    Time: 4 ms (90.47%), Space: 19.3 MB (17.61%) - LeetHub

--- 0125-valid-palindrome/0125-valid-palindrome.py
+++ 0125-valid-palindrome/0125-valid-palindrome.py
@@ -6,4 +6,13 @@ class Solution:
         s = s.replace(" ", "")
         s = re.sub('[^0-9a-zA-Z]+', '', s)
 
-        return s == s[::-1]
+        if s == "":
+            return True
+
+        for i in range(len(s)//2+1):
+            alpha = len(s) - (i+1)
+
+            if s[i] != s[alpha]:
+                return False
+        
+        return True
