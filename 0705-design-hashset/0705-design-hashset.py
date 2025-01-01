e1b6de60253d335e9ccd1bf4ede24c14060084f5
Abhishek Chatterjee <abhishek@imdeepmind.com>
2025-01-01 21:30:06 +0530

    Time: 17 ms (89.74%), Space: 22.3 MB (52.35%) - LeetHub

---
+++ 0705-design-hashset/0705-design-hashset.py
@@ -0,0 +1,22 @@
+class MyHashSet:
+
+    def __init__(self):
+        self.mapper = {}
+        
+
+    def add(self, key: int) -> None:
+        self.mapper[key] = 1
+
+    def remove(self, key: int) -> None:
+        if key in self.mapper:
+            del self.mapper[key]
+
+    def contains(self, key: int) -> bool:
+        return key in self.mapper
+
+
+# Your MyHashSet object will be instantiated and called as such:
+# obj = MyHashSet()
+# obj.add(key)
+# obj.remove(key)
+# param_3 = obj.contains(key)
