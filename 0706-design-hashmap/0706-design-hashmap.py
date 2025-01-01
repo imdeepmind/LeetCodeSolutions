546b8b3c0576b96df511d3625f214ccf0dc0de62
Abhishek Chatterjee <abhishek@imdeepmind.com>
2025-01-01 21:31:34 +0530

    Time: 16 ms (94.71%), Space: 20.9 MB (40.24%) - LeetHub

---
+++ 0706-design-hashmap/0706-design-hashmap.py
@@ -0,0 +1,24 @@
+class MyHashMap:
+
+    def __init__(self):
+        self.mapper = {}
+
+    def put(self, key: int, value: int) -> None:
+        self.mapper[key] = value
+
+    def get(self, key: int) -> int:
+        if key in self.mapper:
+            return self.mapper[key]
+        
+        return -1
+
+    def remove(self, key: int) -> None:
+        if key in self.mapper:
+            del self.mapper[key]
+
+
+# Your MyHashMap object will be instantiated and called as such:
+# obj = MyHashMap()
+# obj.put(key,value)
+# param_2 = obj.get(key)
+# obj.remove(key)
