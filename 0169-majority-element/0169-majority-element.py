9d1cd988e2651b01b75847920fec760acb03e94b
Abhishek Chatterjee <abhishek@imdeepmind.com>
2025-01-01 21:22:33 +0530

    Time: 17 ms (9.08%), Space: 19.4 MB (11.53%) - LeetHub

--- 0169-majority-element/0169-majority-element.py
+++ 0169-majority-element/0169-majority-element.py
@@ -2,17 +2,14 @@ from collections import defaultdict
 
 class Solution:
     def majorityElement(self, nums: List[int]) -> int:
-        freq = defaultdict(int)
+        max_item = 0
+        max_item_value = 0
+        mapper = {}
 
         for num in nums:
-            freq[num] += 1
-        
-        max_item = float('-inf')
-        max_num = None
-
-        for key in freq:
-            if freq[key] > max_item:
-                max_item = freq[key]
-                max_num = key
+            mapper[num] = mapper.get(num, 0) + 1
+            if mapper[num] > max_item:
+                max_item = mapper[num]
+                max_item_value = num
             
-        return max_num
+        return max_item_value
