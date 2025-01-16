2f085608b1ce36d1a989bba079b8725ac0d75c00
Abhishek Chatterjee <abhishek@imdeepmind.com>
2025-01-16 11:47:19 +0530

    Time: 7 ms (90.64%), Space: 17.6 MB (60.18%) - LeetHub

--- 0242-valid-anagram/0242-valid-anagram.py
+++ 0242-valid-anagram/0242-valid-anagram.py
@@ -2,18 +2,27 @@ from collections import defaultdict
 
 class Solution:
     def isAnagram(self, s: str, t: str) -> bool:
-        if len(s) != len(t):
+        sl = len(s)
+        tl = len(t)
+
+        if sl != tl:
             return False
         
         s_freq = defaultdict(int)
         t_freq = defaultdict(int)
 
-        for i in range(len(s)):
-            s_freq[s[i]] += 1
-            t_freq[t[i]] += 1
+        for c in s:
+            s_freq[c] += 1
+        
+        for c in t:
+            t_freq[c] += 1
         
         for key, value in s_freq.items():
             if value != t_freq[key]:
                 return False
-            
+        
+        for key, value in t_freq.items():
+            if value != s_freq[key]:
+                return False
+        
         return True
