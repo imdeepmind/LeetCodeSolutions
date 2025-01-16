4dfe2ea6f7328f1fbf8fae6eaea59b7e4284da58
Abhishek Chatterjee <abhishek@imdeepmind.com>
2025-01-16 12:00:52 +0530

    Time: 3 ms (29.43%), Space: 17.9 MB (24.67%) - LeetHub

--- 0014-longest-common-prefix/0014-longest-common-prefix.py
+++ 0014-longest-common-prefix/0014-longest-common-prefix.py
@@ -1,16 +1,17 @@
 class Solution:
     def longestCommonPrefix(self, strs: List[str]) -> str:
-        if len(strs) == 1:
-            return strs[0]
-        
-        common = ""
-        for index, ch in enumerate(strs[0]):
+        res = ""
+        pivot = strs[0]
+
+        for anchor in pivot:
+            matched = True
             for word in strs[1:]:
-                if len(word) <= index:
-                    return common
-                if word[index] != ch:
-                    return common
-                
-            common += ch
-        
-        return common
+                if not word.startswith(res + anchor):
+                    matched = False
+
+            if matched:
+                res += anchor
+            else:
+                break
+
+        return res
