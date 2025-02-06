f2d51cc385d1452746779590f0e1839a9dd39002
Abhishek Chatterjee <abhishek@imdeepmind.com>
2025-02-07 00:15:34 +0530

    Time: 9 ms (46.21%), Space: 19.5 MB (21.11%) - LeetHub

--- 0125-valid-palindrome/0125-valid-palindrome.py
+++ 0125-valid-palindrome/0125-valid-palindrome.py
@@ -2,14 +2,12 @@ import re
 
 class Solution:
     def isPalindrome(self, s: str) -> bool:
-        s = s.lower()
-        s = s.replace(" ", "")
+        s = s.lower().replace(" ", "").replace("_", "")
         s = re.sub(r"\W", "", s)
-        s = s.replace("_", "")
 
         n = len(s)
 
-        for i in range(n//2):
+        for i in range(n // 2):
             opp = n - i - 1
 
             if s[i] != s[opp]:
