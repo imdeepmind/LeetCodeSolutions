63b42b43e0f003bb25fdbf54f87de7dedcca3095
Abhishek Chatterjee <abhishek@imdeepmind.com>
2025-02-09 21:16:17 +0530

    Time: 0 ms (100%), Space: 17.9 MB (7.34%) - LeetHub

--- 0070-climbing-stairs/0070-climbing-stairs.py
+++ 0070-climbing-stairs/0070-climbing-stairs.py
@@ -1,16 +1,15 @@
 class Solution:
     def climbStairs(self, n: int) -> int:
         ways = {}
-
         def count(n):
             if n == 0: return 1
             if n == 1: return 1
 
             if n in ways: return ways[n]
-            
-            res = count(n-1) + count(n-2)
+
+            res = count(n-1) + count(n - 2)
             ways[n] = res
-            
+
             return res
         
         return count(n)
