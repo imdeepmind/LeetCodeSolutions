04d46a4c107c6253e3d1dcde4a3cf921bb9d6a6c
Abhishek Chatterjee <abhishek@imdeepmind.com>
2025-02-09 19:30:30 +0530

    Time: 3 ms (65.39%), Space: 18.6 MB (84.86%) - LeetHub

--- 0268-missing-number/0268-missing-number.py
+++ 0268-missing-number/0268-missing-number.py
@@ -2,8 +2,10 @@ class Solution:
     def missingNumber(self, nums: List[int]) -> int:
         n = len(nums)
 
-        target = n * (n+1)//2
-
+        target = 0
+        for i in range(n+1):
+            target += i
+        
         current = 0
         for num in nums:
             current += num
