023147be2c3efde5a36918ba9f28a138b1550531
Abhishek Chatterjee <abhishek@imdeepmind.com>
2025-02-09 16:38:14 +0530

    Time: 3 ms (43.09%), Space: 17.7 MB (78.27%) - LeetHub

--- 0202-happy-number/0202-happy-number.py
+++ 0202-happy-number/0202-happy-number.py
@@ -1,31 +1,29 @@
 class Solution:
     def isHappy(self, n: int) -> bool:
         def to_array(n):
-            nums = []
+            arr = []
+
             while n > 0:
-                nums = [n%10] + nums
-                n = n//10
+                digit = n % 10
+                n = n // 10
+
+                arr.append(digit)
             
-            return nums
+            return arr[::-1]
+        
+        mapper = {}
 
-        def productSum(nums):
-            return sum([num**2 for num in nums])
-
-        if n == 1 or n == 7:
-            return True
-
-        nums = to_array(n)
-
-        while len(nums) > 1:
-            n = productSum(nums)
+        while True:
+            sqaured_arr = [x**2 for x in to_array(n)]
+            sum_arr = sum(sqaured_arr)
+            n = sum_arr
             
-            if n == 1 or n == 7:
+            if sum_arr in mapper:
+                break
+
+            mapper[sum_arr] = 1
+
+            if sum_arr == 1:
                 return True
-            nums = to_array(n)
-        
-        if len(nums) == 1 and nums[0] == 1:
-            return True
-        
+
         return False
-        
-        
