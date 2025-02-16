f995ebb51e1cf76f15e4ee833115c4034b6de298
Abhishek Chatterjee <abhishek@imdeepmind.com>
2025-02-16 19:33:09 +0530

    Time: 0 ms (100%), Space: 17.8 MB (61.51%) - LeetHub

--- 0021-merge-two-sorted-lists/0021-merge-two-sorted-lists.py
+++ 0021-merge-two-sorted-lists/0021-merge-two-sorted-lists.py
@@ -7,28 +7,26 @@ class Solution:
     def mergeTwoLists(self, list1: Optional[ListNode], list2: Optional[ListNode]) -> Optional[ListNode]:
         merged = ListNode(0)
         head = merged
-        
+
         while list1 or list2:
-            list1_val = list1.val if list1 else float('inf')
-            list2_val = list2.val if list2 else float('inf')
-
-            if list1_val > list2_val:
-                merged.next = ListNode(list2.val)
-                merged = merged.next
-                list2 = list2.next
+            if list1 and list2:
+                if list1.val > list2.val:
+                    merged.next = ListNode(list2.val)
+                    merged = merged.next
+                    list2 = list2.next
+                else:
+                    merged.next = ListNode(list1.val)
+                    merged = merged.next
+                    list1 = list1.next
             else:
-                merged.next = ListNode(list1.val)
-                merged = merged.next
-                list1 = list1.next
-
-            # if list1:
-            #     merged.next = ListNode(list1.val)
-            #     merged = merged.next
-            #     list1 = list1.next
-            
-            # if list2:
-            #     merged.next = ListNode(list2.val)
-            #     merged = merged.next
-            #     list2 = list2.next
-            
+                if list1:
+                    merged.next = ListNode(list1.val)
+                    merged = merged.next
+                    list1 = list1.next
+                
+                if list2:
+                    merged.next = ListNode(list2.val)
+                    merged = merged.next
+                    list2 = list2.next
+        
         return head.next
