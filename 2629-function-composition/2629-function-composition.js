377cb3ecfdce6cb164dfd57cf1c7497668af84b6
Abhishek Chatterjee <abhishek@imdeepmind.com>
2025-02-16 17:53:30 +0530

    Time: 59 ms (81.79%), Space: 50 MB (68.64%) - LeetHub

--- 2629-function-composition/2629-function-composition.js
+++ 2629-function-composition/2629-function-composition.js
@@ -3,14 +3,14 @@
  * @return {Function}
  */
 var compose = function(functions) {
-    return function(x) {
-        let res = x
+    return (n) => {
+        res = n;
 
-        for (let i = functions.length-1; i >= 0; i--) {
-            res = functions[i](res)
+        for (let i=functions.length-1; i>= 0; i--) {
+            res = functions[i](res);
         }
 
-        return res
+        return res;
     }
 };
 
