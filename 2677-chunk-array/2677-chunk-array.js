d43fa026f8b544803a3a2cf9e34d04a7fd4821ce
Abhishek Chatterjee <abhishek@imdeepmind.com>
2025-02-16 18:18:12 +0530

    Time: 50 ms (84.31%), Space: 51.1 MB (89.01%) - LeetHub

--- 2677-chunk-array/2677-chunk-array.js
+++ 2677-chunk-array/2677-chunk-array.js
@@ -4,19 +4,21 @@
  * @return {Array}
  */
 var chunk = function(arr, size) {
-    if (arr.length <= 0) return arr
-    
-    const chunks = []
-    let i = 0,
-        j = size;
-    
-    while (true) {
-        if (i+1 > arr.length) break
+    const res = [];
+    let batch = []
 
-        chunks.push(arr.slice(i, j))
-        i = j
-        j += size
+    arr.forEach(item => {
+        if (size > batch.length) {
+            batch.push(item)
+        } else {
+            res.push(batch)
+            batch = [item]
+        }
+    })
+
+    if (batch.length > 0) {
+        res.push(batch);
     }
 
-    return chunks
+    return res
 };
