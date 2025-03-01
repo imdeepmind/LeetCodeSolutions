c210615e784b414221713b06d2d3cd3f05d7f519
Abhishek Chatterjee <abhishek@imdeepmind.com>
2025-03-01 12:56:07 +0530

    Time: 82 ms (63.57%), Space: 59.4 MB (63.83%) - LeetHub

--- 2618-check-if-object-instance-of-class/2618-check-if-object-instance-of-class.js
+++ 2618-check-if-object-instance-of-class/2618-check-if-object-instance-of-class.js
@@ -4,8 +4,8 @@
  * @return {boolean}
  */
 var checkIfInstanceOf = function(obj, classFunction) {
-    if(obj === null || obj === undefined || typeof classFunction !== 'function')
-        return false;
+    if (obj === null || obj === undefined || typeof classFunction !== 'function') return false;
+
     return Object(obj) instanceof classFunction
 };
 
