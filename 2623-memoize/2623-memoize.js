48d42e2f20a731e3eb79060244438065623d60a7
Abhishek Chatterjee <abhishek@imdeepmind.com>
2025-03-01 13:20:04 +0530

    Time: 262 ms (88.17%), Space: 94.4 MB (12.36%) - LeetHub

--- 2623-memoize/2623-memoize.js
+++ 2623-memoize/2623-memoize.js
@@ -3,24 +3,25 @@
  * @return {Function}
  */
 function memoize(fn) {
-    const hash = {}
-
+    const cache = {};
     return function(...args) {
-        if (hash[JSON.stringify(args)] === undefined) 
-            hash[JSON.stringify(args)] = fn(...args)
-        
-        return hash[JSON.stringify(args)]
+        const key = JSON.stringify(args)
+
+        if (cache[key] !== undefined) return cache[key];
+
+        cache[key] = fn(...args);
+        return cache[key];
     }
 }
 
 
- 
-//  let callCount = 0;
-//  const memoizedFn = memoize(function (a, b) {
-//  	 callCount += 1;
-//     return a + b;
-//  })
-//  memoizedFn(2, 3) // 5
-//  memoizedFn(2, 3) // 5
-//  console.log(callCount) // 1 
- 
+/** 
+ * let callCount = 0;
+ * const memoizedFn = memoize(function (a, b) {
+ *	 callCount += 1;
+ *   return a + b;
+ * })
+ * memoizedFn(2, 3) // 5
+ * memoizedFn(2, 3) // 5
+ * console.log(callCount) // 1 
+ */
