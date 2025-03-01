a77a45e691369b644a3595de00ab80bb55d0c890
Abhishek Chatterjee <abhishek@imdeepmind.com>
2025-03-01 15:23:22 +0530

    Time: 63 ms (82.9%), Space: 54.9 MB (5.5%) - LeetHub

--- 2721-execute-asynchronous-functions-in-parallel/2721-execute-asynchronous-functions-in-parallel.js
+++ 2721-execute-asynchronous-functions-in-parallel/2721-execute-asynchronous-functions-in-parallel.js
@@ -2,33 +2,24 @@
  * @param {Array<Function>} functions
  * @return {Promise<any>}
  */
+var promiseAll = function(functions) {
+    return new Promise((resolve, reject) => {
+        const arr = Array.from(functions.length).fill(0);
+        let count = 0;
 
-var promiseAll = async function(functions) {
-    return new Promise((resolve,reject)=>{
-        if(functions.length === 0) {
-            resolve([]);
-            return;
-        }
-        const arr=new Array(functions.length);
-        arr.fill(null);
-        let count=0
-        functions.forEach(async(fn,i)=>{
-            try{
-                arr[i]=await fn()
-                count++
-            
-                if(count===functions.length){
-                    resolve(arr);
-                }
+        functions.forEach(async (func, index) => {
+            try {
+                arr[index] = await func();
+                count++;
+                
+                if (count === functions.length) resolve(arr);
+            } catch (error) {
+                reject(error)
             }
-            catch(e){
-                reject(e);
-            }
-        
-        });
-    });
+        })
+
+    })
 };
-
 
 /**
  * const promise = promiseAll([() => new Promise(res => res(42))])
