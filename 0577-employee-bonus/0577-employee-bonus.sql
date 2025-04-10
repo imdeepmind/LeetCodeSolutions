de4bd69b356da501b1c0f652f153636d18e876a3
Abhishek Chatterjee <abhishek@imdeepmind.com>
2025-04-10 16:19:59 +0530

    Time: 318 ms (82.89%), Space: 0B (100%) - LeetHub

--- 0577-employee-bonus/0577-employee-bonus.sql
+++ 0577-employee-bonus/0577-employee-bonus.sql
@@ -1,8 +1,7 @@
 -- Write your PostgreSQL query statement below
-SELECT
-    E.name,
-    B.bonus
+SELECT 
+    E.name, B.bonus
 FROM Employee E
 LEFT JOIN Bonus B
-    ON E.empID = B.empId
+ON B.empId = E.empId
 WHERE B.bonus < 1000 OR B.bonus IS NULL
