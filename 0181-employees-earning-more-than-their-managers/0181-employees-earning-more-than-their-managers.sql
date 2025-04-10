8070815a89f7708d45648b377303c0d7cc8f726e
Abhishek Chatterjee <abhishek@imdeepmind.com>
2025-04-10 15:47:06 +0530

    Time: 242 ms (82.4%), Space: 0B (100%) - LeetHub

--- 0181-employees-earning-more-than-their-managers/0181-employees-earning-more-than-their-managers.sql
+++ 0181-employees-earning-more-than-their-managers/0181-employees-earning-more-than-their-managers.sql
@@ -2,7 +2,6 @@
 SELECT 
     E1.name as Employee
 FROM Employee E1
-INNER JOIN Employee E2
-    ON E1.managerId = E2.id
-WHERE 
-    E1.salary > E2.salary
+LEFT JOIN Employee E2
+ON E1.managerId = E2.id
+WHERE E1.salary > E2.salary
