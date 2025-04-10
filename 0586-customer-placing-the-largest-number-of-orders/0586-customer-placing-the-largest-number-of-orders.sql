f7347af8b213c35975c96b1ff1e937b95a7ddda2
Abhishek Chatterjee <abhishek@imdeepmind.com>
2025-04-10 16:27:38 +0530

    Time: 246 ms (86.45%), Space: 0B (100%) - LeetHub

--- 0586-customer-placing-the-largest-number-of-orders/0586-customer-placing-the-largest-number-of-orders.sql
+++ 0586-customer-placing-the-largest-number-of-orders/0586-customer-placing-the-largest-number-of-orders.sql
@@ -1,5 +1,7 @@
 -- Write your PostgreSQL query statement below
-SELECT customer_number FROM Orders
-GROUP BY customer_number
-ORDER BY COUNT(customer_number) DESC
+SELECT 
+    O.customer_number
+FROM Orders O
+GROUP BY O.customer_number
+ORDER BY COUNT(O.customer_number) DESC
 LIMIT 1
