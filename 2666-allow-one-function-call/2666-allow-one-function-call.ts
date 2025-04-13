4461de318e6a3ab28288e4b1a7be81a84ef2cee0
Abhishek Chatterjee <abhishek@imdeepmind.com>
2025-04-13 18:40:32 +0530

    Time: 53 ms (30.32%), Space: 55.5 MB (53.16%) - LeetHub

---
+++ 2666-allow-one-function-call/2666-allow-one-function-call.ts
@@ -0,0 +1,24 @@
+type JSONValue = null | boolean | number | string | JSONValue[] | { [key: string]: JSONValue };
+type OnceFn = (...args: JSONValue[]) => JSONValue | undefined
+
+function once(fn: Function): OnceFn {
+    let isCalled = false;
+
+    return function (...args) {
+        if (isCalled) {
+            return
+        }
+
+        isCalled = true;
+        return fn(...args);
+        
+    };
+}
+
+/**
+ * let fn = (a,b,c) => (a + b + c)
+ * let onceFn = once(fn)
+ *
+ * onceFn(1,2,3); // 6
+ * onceFn(2,3,6); // returns undefined without calling fn
+ */
