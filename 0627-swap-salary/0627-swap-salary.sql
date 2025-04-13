71b5479682fbe39722bb08aa2de21dfe66efed12
Abhishek Chatterjee <abhishek@imdeepmind.com>
2025-04-13 16:58:20 +0530

    Time: 228 ms (68.68%), Space: 0B (100%) - LeetHub

--- 0627-swap-salary/0627-swap-salary.sql
+++ 0627-swap-salary/0627-swap-salary.sql
@@ -1,3 +1,3 @@
 -- Write your PostgreSQL query statement below
-UPDATE Salary 
-SET sex = CASE WHEN sex = 'f' THEN 'm' ELSE 'f' END
+UPDATE Salary S
+SET sex = CASE WHEN S.sex = 'f' THEN 'm' ELSE 'f' END
