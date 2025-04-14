e48b965e285d80def2cd04122a63ab6accb62b86
Abhishek Chatterjee <abhishek@imdeepmind.com>
2025-04-14 17:15:44 +0530

    Time: 0 ms (100%), Space: 17.8 MB (80.89%) - LeetHub

--- 0020-valid-parentheses/0020-valid-parentheses.py
+++ 0020-valid-parentheses/0020-valid-parentheses.py
@@ -1,21 +1,21 @@
 class Solution:
     def isValid(self, s: str) -> bool:
+        start_params = []
+        end_params = []
+
         opp = {
             ')': '(',
             '}': '{',
-            ']': '['
+            ']': '[' 
         }
 
-        left_stack = []
-        right_stack = []
-
         for c in s:
-            if c in ("(", "{", "["):
-                left_stack.append(c)
+            if c in ['(', '{', '[']:
+                start_params.append(c)
             else:
-                if left_stack and left_stack[-1] == opp[c]:
-                    del left_stack[-1]
+                if start_params and start_params[-1] == opp[c]:
+                    del start_params[-1]
                 else:
-                    right_stack.append(c)
-        
-        return not (len(left_stack) > 0 or len(right_stack) > 0)
+                    end_params.append(c)
+
+        return len(start_params) == 0 and len(end_params) == 0
