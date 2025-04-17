36bd2cff397078460e905b3c187920ffcf7e4ad2
Abhishek Chatterjee <abhishek@imdeepmind.com>
2025-04-17 20:19:24 +0530

    Time: 259 ms (70.94%), Space: 0B (100%) - LeetHub

--- 0196-delete-duplicate-emails/0196-delete-duplicate-emails.sql
+++ 0196-delete-duplicate-emails/0196-delete-duplicate-emails.sql
@@ -1,13 +1,7 @@
 -- Write your PostgreSQL query statement below
-DELETE FROM person
-    WHERE id NOT IN (
-        SELECT MIN(id)
-        FROM person
-        GROUP BY email
-    )
-    AND email IN (
-        SELECT email
-        FROM person
-        GROUP BY email
-        HAVING COUNT(*) > 1
-    );
+DELETE FROM Person P
+WHERE P.email IN (
+    SELECT P.email FROM Person P GROUP BY P.email HAVING COUNT(P.*) > 1
+) AND P.id NOT IN (
+    SELECT MIN(p.id) FROM Person P GROUP BY P.email HAVING COUNT(P.*) > 1
+)
