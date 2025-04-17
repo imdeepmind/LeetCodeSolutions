72ff318bcd9d7de6861abd4615c7450ad00cd8d1
Abhishek Chatterjee <abhishek@imdeepmind.com>
2025-04-17 20:26:47 +0530

    Time: 569 ms (66.05%), Space: 0B (100%) - LeetHub

--- 0511-game-play-analysis-i/0511-game-play-analysis-i.sql
+++ 0511-game-play-analysis-i/0511-game-play-analysis-i.sql
@@ -1,6 +1,6 @@
 -- Write your PostgreSQL query statement below
 SELECT
     A.player_id,
-    MIN(A.event_date) as first_login
+    MIN(A.event_date) AS first_login
 FROM Activity A
 GROUP BY A.player_id
