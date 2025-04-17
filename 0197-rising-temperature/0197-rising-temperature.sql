fae18856dd5d39f942018add6096eef56536cb83
Abhishek Chatterjee <abhishek@imdeepmind.com>
2025-04-17 20:24:08 +0530

    Time: 224 ms (84.77%), Space: 0B (100%) - LeetHub

--- 0197-rising-temperature/0197-rising-temperature.sql
+++ 0197-rising-temperature/0197-rising-temperature.sql
@@ -3,6 +3,5 @@ SELECT
     W2.id
 FROM Weather W1
 LEFT JOIN Weather W2
-    ON DATE(W1.recordDate) + 1 = W2.recordDate
-WHERE 
-    W2.temperature > W1.temperature
+ON W2.recordDate = DATE(W1.recordDate) + 1
+WHERE W2.temperature > W1.temperature
