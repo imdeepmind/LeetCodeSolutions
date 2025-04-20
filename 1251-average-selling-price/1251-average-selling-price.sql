5e2129396745501a7af6b56e3bcd5e975ae8860d
Abhishek Chatterjee <abhishek@imdeepmind.com>
2025-04-20 22:38:21 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 1251-average-selling-price/1251-average-selling-price.sql
+++ 1251-average-selling-price/1251-average-selling-price.sql
@@ -1,17 +1,14 @@
 -- Write your PostgreSQL query statement below
-SELECT product_id, COALESCE(ROUND(SUM(total_cost) / SUM(units)::decimal, 2), 0) as average_price FROM (
-    SELECT P.product_id, US.units, P.price, US.units * P.price AS total_cost FROM Prices P
-    LEFT JOIN UnitsSold US
-    ON US.product_id = P.product_id AND US.purchase_date BETWEEN P.start_date AND p.end_date
-    WHERE P.product_id IS NOT NULL
-) as tmp
-GROUP BY product_id
--- GROUP BY P.product_id, US.product_id
-
-
--- SELECT * FROM (
---     SELECT P.product_id, US.units, P.price, US.units * P.price AS total_cost FROM Prices P
---     LEFT JOIN UnitsSold US
---     ON US.product_id = P.product_id AND US.purchase_date BETWEEN P.start_date AND p.end_date
---     WHERE P.product_id IS NOT NULL
--- ) as tmp
+SELECT 
+    P.product_id,
+    ROUND(SUM(P.total_earning) / SUM(P.units)::decimal, 2) AS "average_price"
+FROM (
+    SELECT
+        P.product_id,
+        US.units * P.price AS "total_earning",
+        US.units
+    FROM Prices P
+    INNER JOIN UnitsSold US
+    ON US.product_id = P.product_id AND US.purchase_date BETWEEN P.start_date AND P.end_date
+) as P
+GROUP BY P.product_id
