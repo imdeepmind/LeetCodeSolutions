ff91b0b0deb2e78dc175f460d0a112d903da4eda
Abhishek Chatterjee <abhishek@imdeepmind.com>
2025-04-21 15:16:19 +0530

    Time: 2 ms (84.26%), Space: 17.6 MB (90.36%) - LeetHub

--- 0013-roman-to-integer/0013-roman-to-integer.py
+++ 0013-roman-to-integer/0013-roman-to-integer.py
@@ -3,51 +3,49 @@ class Solution:
         mapper = {
             'I': 1,
             'V': 5,
-            'X': 10,
-            'L': 50,
-            'C': 100,
-            'D': 500,
-            'M': 1000
+            "X": 10,
+            "L": 50,
+            "C": 100,
+            "D": 500,
+            "M": 1000
         }
 
-        res = 0
+        val = 0
+        i = 0
 
-        l = 0
+        while len(s) > i:
+            c = s[i]
+            nxt_c = s[i+1] if len(s)-1 > i else 'NA'
 
-        while len(s) > l:
-            c = s[l]
+            if c == 'I':
+                if nxt_c == 'V':
+                    val += 4
+                    i += 2
+                    continue
+                elif nxt_c == "X":
+                    val += 9
+                    i += 2
+                    continue
+            elif c == "X":
+                if nxt_c == "L":
+                    val += 40
+                    i += 2
+                    continue
+                elif nxt_c == "C":
+                    val += 90
+                    i += 2
+                    continue
+            elif c == "C":
+                if nxt_c == "D":
+                    val += 400
+                    i += 2
+                    continue
+                elif nxt_c == "M": 
+                    val += 900
+                    i += 2
+                    continue
+            
+            val += mapper[c]
+            i += 1
 
-            if len(s) > l+1:
-                nxt = s[l+1]
-                if c == "I":
-                    if nxt == "V":
-                        res += 4
-                        l += 2
-                        continue
-                    elif nxt == "X":
-                        res += 9
-                        l += 2
-                        continue
-                elif c == "X":
-                    if nxt == "L":
-                        res += 40
-                        l += 2
-                        continue
-                    elif nxt == "C":
-                        res += 90
-                        l += 2
-                        continue
-                elif c == "C":
-                    if nxt == "D":
-                        res += 400
-                        l += 2
-                        continue
-                    elif nxt == "M":
-                        res += 900
-                        l += 2
-                        continue
-                
-            res += mapper[c]
-            l += 1
-
-        return res
+        return val
