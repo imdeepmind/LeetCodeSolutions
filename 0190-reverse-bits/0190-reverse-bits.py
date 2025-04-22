b561c4738930ed1a5955261ff30f974e6fc7b37f
Abhishek Chatterjee <abhishek@imdeepmind.com>
2025-04-22 21:56:56 +0530

    Time: 49 ms (7.4%), Space: 18 MB (15.28%) - LeetHub

--- 0190-reverse-bits/0190-reverse-bits.py
+++ 0190-reverse-bits/0190-reverse-bits.py
@@ -1,7 +1,7 @@
 class Solution:
     def reverseBits(self, n: int) -> int:
         res = 0
-        
+
         for i in range(32):
             last = (n >> i) & 1
             res += last << (31 - i)
