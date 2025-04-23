ee46f191595aff4904bd5503e9474f6afd3733eb
Abhishek Chatterjee <abhishek@imdeepmind.com>
2025-04-23 12:17:07 +0530

    Time: 39 ms (8.29%), Space: 19.5 MB (12.82%) - LeetHub

--- 0042-trapping-rain-water/0042-trapping-rain-water.py
+++ 0042-trapping-rain-water/0042-trapping-rain-water.py
@@ -1,37 +1,35 @@
 class Solution:
     def trap(self, height: List[int]) -> int:
-        left_maxes = []
-        left_max = 0
+        left_pillars = []
+        right_pillars = []
+
+        left_max = float('-inf')
+        right_max = float('-inf')
 
         for i in range(len(height)):
             if i == 0:
-                left_maxes.append(0)
+                left_pillars.append(0)
+                left_max = height[i]
                 continue
-
-            if height[i-1] > left_max:
-                left_max = height[i-1]
-                left_maxes.append(height[i-1])
-            else:
-                left_maxes.append(left_max)
-
-        right_maxes = []
-        right_max = 0
-
+            
+            left_max = max(height[i-1], left_max)
+            left_pillars.append(left_max)
+        
         for i in range(len(height)-1, -1, -1):
             if i == len(height) - 1:
-                right_maxes.append(0)
+                right_pillars.append(0)
+                right_max = height[i]
                 continue
+            
+            right_max = max(right_max, height[i+1])
+            right_pillars.append(right_max)
 
-            if height[i+1] > right_max:
-                right_max = height[i+1]
-                right_maxes.append(height[i+1])
-            else:
-                right_maxes.append(right_max)
-
-        right_maxes = right_maxes[::-1]
+        right_pillars = right_pillars[::-1]
 
         area = 0
+
         for i in range(len(height)):
-            area += max(0, min(left_maxes[i], right_maxes[i]) - height[i])
+            area += max(0, min(left_pillars[i], right_pillars[i]) - height[i])
 
         return area
+
