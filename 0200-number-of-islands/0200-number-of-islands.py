f31a3b68ca3da9461b00153f49412c26739f9a43
Abhishek Chatterjee <abhishek@imdeepmind.com>
2025-04-27 19:05:38 +0530

    Time: 280 ms (23.94%), Space: 28.5 MB (8.91%) - LeetHub

--- 0200-number-of-islands/0200-number-of-islands.py
+++ 0200-number-of-islands/0200-number-of-islands.py
@@ -1,34 +1,40 @@
 class Solution:
     def numIslands(self, grid: List[List[str]]) -> int:
-        ROWS, COLS = len(grid), len(grid[0])
-        no_of_islands = 0
+        if not grid:
+            return 0
+
+        islands = 0
         visited = set()
-        directions = [[1, 0], [-1, 0], [0, 1], [0, -1]]
+        directions = [[1, 0], [0, 1], [-1, 0], [0, -1]]
 
-        def dfs(i, j):
-            if i < 0 or j < 0:
-                return
-            
-            if i >= ROWS or j >= COLS:
-                return 
-            
-            if grid[i][j] == "0":
-                return
-            
-            if (i, j) in visited:
-                return
+        ROWS, COLS = len(grid), len(grid[0])
 
-            if grid[i][j] == "1":
-                visited.add((i, j))
-                for dr, dc in directions:
-                    dfs(i + dr, j + dc)
+        def dfs(r, c):
+            if r < 0 or c < 0:
+                return
+            
+            if r >= ROWS or c >= COLS:
+                return
+            
+            if (r, c) in visited:
+                return
+            
+            if grid[r][c] != "1":
+                return
+            
+            visited.add((r, c))
+
+            for dr, dc in directions:
+                dfs(r + dr, c + dc)
             
             return
-                
-        for i in range(ROWS):
-            for j in range(COLS):
-                if grid[i][j] == "1" and (i,j) not in visited:
-                    dfs(i, j)
-                    no_of_islands += 1
 
-        return no_of_islands
+
+        for r in range(ROWS):
+            for c in range(COLS):
+                if grid[r][c] == "1" and (r, c) not in visited:
+                    dfs(r, c)
+                    islands += 1
+        
+        return islands
+
