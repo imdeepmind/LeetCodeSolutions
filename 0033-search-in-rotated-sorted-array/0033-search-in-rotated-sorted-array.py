8ebf020a80fc2bb50baaaabfeea3189a118041a6
Abhishek Chatterjee <abhishek@imdeepmind.com>
2025-04-29 21:01:39 +0530

    Time: 0 ms (100%), Space: 18.3 MB (22.71%) - LeetHub

--- 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.py
+++ 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.py
@@ -3,10 +3,11 @@ class Solution:
         l, r = 0, len(nums) - 1
 
         while l <= r:
-            mid = (l + r) // 2
-            if target == nums[mid]:
-                return mid
+            mid = l + ((r-l) // 2)
 
+            if nums[mid] == target:
+                return mid
+            
             if nums[l] <= nums[mid]:
                 if target > nums[mid] or target < nums[l]:
                     l = mid + 1
@@ -18,4 +19,5 @@ class Solution:
                     r = mid - 1
                 else:
                     l = mid + 1
+        
         return -1
