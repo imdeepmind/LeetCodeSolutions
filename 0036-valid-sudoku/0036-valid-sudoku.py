771b0b835793cd50edac1f302804cf8a207dcc64
Abhishek Chatterjee <abhishek@imdeepmind.com>
2025-05-06 16:08:58 +0530

    Time: 3 ms (83.63%), Space: 18 MB (25.06%) - LeetHub

--- 0036-valid-sudoku/0036-valid-sudoku.py
+++ 0036-valid-sudoku/0036-valid-sudoku.py
@@ -1,52 +1,55 @@
 class Solution:
-    def containsDuplicate(self, nums: List[int]) -> bool:
-        hash = {}
-
-        for num in nums:
-            if num != ".":
-                if num not in hash:
-                    hash[num] = 0
-                else:
-                    return True
-        
-        return False
-    def getAllColumns(self, board):
-        res = []
-        for i in range(9):
-            level = []
-            for j in range(9):
-                level.append(board[j][i])
-
-            res.append(level)
-
-        return res
-
-    def getAllSubboxes(self, board):
-        res = []
-        for i in range(0, 9, 3):
-            sub_rows = board[i:i+3]
-
-            for j in range(0, 9, 3):
-                level = []
-                for x in range(3):
-                    sub_columns = sub_rows[x][j:j+3]
-                    level += sub_columns
-
-                res.append(level)
-
-        return res
-
     def isValidSudoku(self, board: List[List[str]]) -> bool:
-        for row in board:
-            if self.containsDuplicate(row):
-                return False
+        if not board: return True
+
+        def is_column_valid():
+            for column in board:
+                mapper = {}
+
+                for item in column:
+                    if item in (",", "."): continue
+                    if item in mapper: return False
+                    mapper[item] = 1
+            
+            return True
         
-        for column in self.getAllColumns(board):
-            if self.containsDuplicate(column):
-                return False
+        def is_valid_row():
+            ROWS, COLS = len(board), len(board[0])
 
-        for sub in self.getAllSubboxes(board):
-            if self.containsDuplicate(sub):
-                return False
+            for c in range(COLS):
+                mapper = {}
+                for r in range(ROWS):
+                    alpha = board[r][c]
 
-        return True
+                    if alpha in (",", "."): continue
+                    if alpha in mapper: return False
+                    mapper[alpha] = 1
+            
+            return True
+        
+        def is_valid_sub_box():
+            res = []
+            for i in range(0, 9, 3):
+                sub_rows = board[i:i+3]
+
+                for j in range(0, 9, 3):
+                    level = []
+                    for x in range(3):
+                        sub_columns = sub_rows[x][j:j+3]
+                        level += sub_columns
+
+                    res.append(level)
+            
+            for column in res:
+                mapper = {}
+
+                for item in column:
+                    if item in (",", "."): continue
+                    if item in mapper: return False
+                    mapper[item] = 1
+            
+            return True
+
+        
+        return is_column_valid() and is_valid_row() and is_valid_sub_box()
+                    
