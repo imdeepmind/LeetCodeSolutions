571630afb76608a338c41bbe8fd4ff452a23d691
Abhishek Chatterjee <abhishek@imdeepmind.com>
2025-07-19 17:46:53 +0530

    Time: 7 ms (89.15%), Space: 17.9 MB (64.14%) - LeetHub

--- 0242-valid-anagram/0242-valid-anagram.py
+++ 0242-valid-anagram/0242-valid-anagram.py
@@ -1,21 +1,8 @@
+from collections import Counter
+
 class Solution:
     def isAnagram(self, s: str, t: str) -> bool:
         if len(s) != len(t):
             return False
         
-        mapper = {}
-
-        for c in s:
-            mapper[c] = mapper.get(c, 0) + 1
-        
-        for c in t:
-            if c not in mapper:
-                return False
-            
-            if mapper[c] > 0:
-                mapper[c] -= 1
-
-                if mapper[c] == 0:
-                    del mapper[c]
-        
-        return len(mapper) == 0
+        return Counter(s) == Counter(t)
