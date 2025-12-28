eda2f1ae227df3a6277684932056c95ae06b0faa
Abhishek Chatterjee <abhishek@imdeepmind.com>
2025-12-28 12:03:47 +0530

    Problem: 0242-valid-anagram, Date: 12-28-2025, Space and Time: 17.7 MB (89.6%) 11 ms (73.85%), Language: .py

--- 0242-valid-anagram/0242-valid-anagram.py
+++ 0242-valid-anagram/0242-valid-anagram.py
@@ -1,8 +1,18 @@
-from collections import Counter
-
 class Solution:
     def isAnagram(self, s: str, t: str) -> bool:
         if len(s) != len(t):
             return False
         
-        return Counter(s) == Counter(t)
+        freq = {}
+
+        for c in s:
+            freq[c] = 1 + freq.get(c, 0)
+        
+        for c in t:
+            freq[c] = freq.get(c, 0) - 1
+        
+        for k, v in freq.items():
+            if v != 0:
+                return False
+        
+        return True
