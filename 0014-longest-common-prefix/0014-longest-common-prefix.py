8957ce2e235a9d82b260b59a930965cb00b36feb
Abhishek Chatterjee <abhishek@imdeepmind.com>
2025-12-28 12:37:44 +0530

    Problem: 0014-longest-common-prefix, Date: 12-28-2025, Space and Time: 17.2 MB (99.83%) 3 ms (27.53%), Language: .py

--- 0014-longest-common-prefix/0014-longest-common-prefix.py
+++ 0014-longest-common-prefix/0014-longest-common-prefix.py
@@ -1,18 +1,21 @@
 class Solution:
     def longestCommonPrefix(self, strs: List[str]) -> str:
         pivot = strs[0]
-
-        resp = ""
+        output = ""
 
         for c in pivot:
+            delta = output + c
             is_match = True
+
             for word in strs[1:]:
-                if not word.startswith(resp + c):
+                if is_match and not word.startswith(delta):
                     is_match = False
-                
+            
             if is_match:
-                resp += c
+                output = delta
             else:
                 break
             
-        return resp
+        return output
+            
+
