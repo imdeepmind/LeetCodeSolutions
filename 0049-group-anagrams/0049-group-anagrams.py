3eff2029481f30e2affd9473fcb7f3af05d59518
Abhishek Chatterjee <abhishek@imdeepmind.com>
2026-01-01 10:34:07 +0530

    Problem: 0049-group-anagrams, Date: 01-01-2026, Space and Time: 20.1 MB (93.6%) 15 ms (50.14%), Language: .py

--- 0049-group-anagrams/0049-group-anagrams.py
+++ 0049-group-anagrams/0049-group-anagrams.py
@@ -1,18 +1,18 @@
 from collections import defaultdict
 
 class Solution:
-    def generateKey(self, word):
-        return "".join(sorted(word))
-
     def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
-        _hash = defaultdict(list)
+        def getKey(word):
+            return "".join(sorted(word))
+        
+        hash = defaultdict(list)
 
         for word in strs:
-            _hash[self.generateKey(word)].append(word)
+            hash[getKey(word)].append(word)
         
-        res = []
+        resp = []
 
-        for key, value in _hash.items():
-            res.append(value)
-
-        return res
+        for key, value in hash.items():
+            resp.append(value)
+        
+        return resp
