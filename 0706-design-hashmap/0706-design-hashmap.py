f5e8d3314fbca321c6aa6c5ba8ac1662a7f341ef
Abhishek Chatterjee <abhishek@imdeepmind.com>
2026-01-01 12:15:49 +0530

    Problem: 0706-design-hashmap, Date: 01-01-2026, Space and Time: 22.8 MB (24.57%) 122 ms (17.39%), Language: .py

--- 0706-design-hashmap/0706-design-hashmap.py
+++ 0706-design-hashmap/0706-design-hashmap.py
@@ -1,20 +1,46 @@
+class ListNode:
+    def __init__(self, key, value=None):
+        self.key = key
+        self.value = value
+        self.next = None
+
 class MyHashMap:
 
     def __init__(self):
-        self.mapper = {}
-
+        self.__set = [ListNode(0) for _ in range(10**4)]
+        
     def put(self, key: int, value: int) -> None:
-        self.mapper[key] = value
+        curr = self.__set[key % len(self.__set)]
 
+        while curr.next:
+            if curr.next.key == key:
+                curr.next.value = value
+                return
+            
+            curr = curr.next
+        
+        curr.next = ListNode(key, value)
+        
     def get(self, key: int) -> int:
-        if key in self.mapper:
-            return self.mapper[key]
+        curr = self.__set[key % len(self.__set)]
+
+        while curr.next:
+            if curr.next.key == key:
+                return curr.next.value
+            
+            curr = curr.next
         
         return -1
 
     def remove(self, key: int) -> None:
-        if key in self.mapper:
-            del self.mapper[key]
+        curr = self.__set[key % len(self.__set)]
+
+        while curr.next:
+            if curr.next.key == key:
+                curr.next = curr.next.next
+                return
+            
+            curr = curr.next
 
 
 # Your MyHashMap object will be instantiated and called as such:
