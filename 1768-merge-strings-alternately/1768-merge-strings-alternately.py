020bbd4559c426a843d5ab93bdad7a21e50396a4
Abhishek Chatterjee <abhishek@imdeepmind.com>
2026-01-04 19:33:19 +0530

    Problem: 1768-merge-strings-alternately, Date: 01-04-2026, Space and Time: 17.2 MB (95.72%) 34 ms (77.73%), Language: .py

--- 1768-merge-strings-alternately/1768-merge-strings-alternately.py
+++ 1768-merge-strings-alternately/1768-merge-strings-alternately.py
@@ -1,16 +1,16 @@
 class Solution:
     def mergeAlternately(self, word1: str, word2: str) -> str:
-        new = ""
-        i, j = 0, 0
-        n1, n2 = len(word1), len(word2)
+        n1, n2 = len(word1)-1, len(word2)-1
+        p, q = 0, 0
+        res = ""
 
-        while n1 > i or n2 > j:
-            if n1 > i:
-                new += word1[i]
-                i += 1
+        while n1 >= p or n2 >= q:
+            if n1 >= p:
+                res += word1[p]
+                p += 1
             
-            if n2 > j:
-                new += word2[j]
-                j += 1
+            if n2 >= q:
+                res += word2[q]
+                q += 1
         
-        return new
+        return res
