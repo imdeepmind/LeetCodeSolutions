3a6ce3eb461466efdfb2d0d96708d7b03634302a
Abhishek Chatterjee <abhishek@imdeepmind.com>
2026-01-15 20:08:32 +0530

    Problem: 0242-valid-anagram, Date: 01-15-2026, Space and Time: 19.5 MB (12.06%) 7 ms (90.74%), Language: .py

--- 0242-valid-anagram/0242-valid-anagram.py
+++ 0242-valid-anagram/0242-valid-anagram.py
@@ -2,12 +2,15 @@ from collections import defaultdict
 
 class Solution:
     def isAnagram(self, s: str, t: str) -> bool:
-        if len(s) != len(t): return False
+        n1, n2 = len(s), len(t)
 
+        if n1 != n2:
+            return False
+        
         freq = defaultdict(int)
 
         for c in s:
-            freq[c] = freq.get(c, 0) + 1
+            freq[c] += 1
         
         for c in t:
             freq[c] -= 1
@@ -17,4 +20,3 @@ class Solution:
                 return False
         
         return True
-        
