a948e4e2d34367a85c8fb7dbc5c656486f5d24ed
Abhishek Chatterjee <abhishek@imdeepmind.com>
2026-01-17 11:17:11 +0530

    Problem: 0169-majority-element, Date: 01-17-2026, Space and Time: 21.2 MB (9.96%) 5 ms (65.43%), Language: .py

--- 0169-majority-element/0169-majority-element.py
+++ 0169-majority-element/0169-majority-element.py
@@ -1,18 +1,11 @@
-from collections import defaultdict
-
 class Solution:
     def majorityElement(self, nums: List[int]) -> int:
-        mapper = defaultdict(int)
+        res, count = 0, 0
 
         for num in nums:
-            mapper[num] += 1
+            if count == 0:
+                res = num
+            
+            count += 1 if res == num else -1
         
-        max_value = 0
-        max_item = None
-
-        for key, value in mapper.items():
-            if value > max_value:
-                max_value = value
-                max_item = key
-        
-        return max_item
+        return res
