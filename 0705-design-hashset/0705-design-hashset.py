c0ca9d81530bb0ec44fd20323c09e9721e375c5a
Abhishek Chatterjee <abhishek@imdeepmind.com>
2026-01-17 11:27:17 +0530

    Problem: 0705-design-hashset, Date: 01-17-2026, Space and Time: 26.1 MB (24.66%) 69 ms (39.3%), Language: .py

--- 0705-design-hashset/0705-design-hashset.py
+++ 0705-design-hashset/0705-design-hashset.py
@@ -1,4 +1,4 @@
-class ListNode:
+class Node:
     def __init__(self, key):
         self.key = key
         self.next = None
@@ -6,41 +6,41 @@ class ListNode:
 class MyHashSet:
 
     def __init__(self):
-        self.__set = [ListNode(0) for _ in range(10**4)]
+        self.__res = [Node(0) for _ in range(10**4)]
 
     def add(self, key: int) -> None:
-        cur = self.__set[key % len(self.__set)]
+        cur = self.__res[key % len(self.__res)]
 
         while cur.next:
             if cur.next.key == key:
                 return
-            
+
             cur = cur.next
         
-        cur.next = ListNode(key)
+        cur.next = Node(key)
         
+
     def remove(self, key: int) -> None:
-        cur = self.__set[key % len(self.__set)]
+        cur = self.__res[key % len(self.__res)]
 
         while cur.next:
             if cur.next.key == key:
                 cur.next = cur.next.next
                 return
-            
+
             cur = cur.next
-        
-        return
 
     def contains(self, key: int) -> bool:
-        cur = self.__set[key % len(self.__set)]
+        cur = self.__res[key % len(self.__res)]
 
         while cur.next:
             if cur.next.key == key:
                 return True
-            
+
             cur = cur.next
         
         return False
+
 
 # Your MyHashSet object will be instantiated and called as such:
 # obj = MyHashSet()
