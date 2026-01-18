516fb7f6194cbc4025a5e929dffe149d4aceb15f
Abhishek Chatterjee <abhishek@imdeepmind.com>
2026-01-18 12:37:01 +0530

    Problem: 0125-valid-palindrome, Date: 01-18-2026, Space and Time: 20.7 MB (19.06%) 3 ms (99.08%), Language: .py

--- 0125-valid-palindrome/0125-valid-palindrome.py
+++ 0125-valid-palindrome/0125-valid-palindrome.py
@@ -1,14 +1,13 @@
-import re
-
 class Solution:
     def isPalindrome(self, s: str) -> bool:
         s = s.lower()
         s = s.replace(" ", "")
-        s = re.sub(r"[^a-zA-Z0-9]", "", s)
+        s = s.replace("_", "")
+        s = re.sub(r'\W+', "", s)
 
         start, end = 0, len(s) - 1
 
-        while end > start:
+        while end >= start:
             if s[start] != s[end]:
                 return False
             
