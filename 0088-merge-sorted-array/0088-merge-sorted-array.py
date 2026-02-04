d53a07f8fd85d92d5350c78b045cb85a591ef3a9
Abhishek Chatterjee <abhishek@imdeepmind.com>
2026-02-04 21:39:18 +0530

    Problem: 0088-merge-sorted-array, Date: 02-04-2026, Space and Time: 19.3 MB (40.01%) 0 ms (100%), Language: .py

--- 0088-merge-sorted-array/0088-merge-sorted-array.py
+++ 0088-merge-sorted-array/0088-merge-sorted-array.py
@@ -3,15 +3,15 @@ class Solution:
         """
         Do not return anything, modify nums1 in-place instead.
         """
-        i, j = m - 1, n - 1
-        k = m + n - 1
+        a1, a2 = m - 1, n - 1
+        k = len(nums1) - 1
 
-        while j >= 0:
-            if i >= 0 and nums1[i] > nums2[j]:
-                nums1[k] = nums1[i]
-                i -= 1
+        while a2 >= 0:
+            if a1 >= 0 and nums1[a1] > nums2[a2]:
+                nums1[k] = nums1[a1]
+                a1 -= 1
             else:
-                nums1[k] = nums2[j]
-                j -= 1
-            
+                nums1[k] = nums2[a2]
+                a2 -= 1
+
             k -= 1
