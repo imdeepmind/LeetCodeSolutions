613f0e7b5c7792bfe80d86bc97d1d4f13088e8aa
Abhishek Chatterjee <abhishek@imdeepmind.com>
2026-02-07 22:05:24 +0530

    Problem: 0232-implement-queue-using-stacks, Date: 02-07-2026, Space and Time: 19.6 MB (9.22%) 0 ms (100%), Language: .py

--- 0232-implement-queue-using-stacks/0232-implement-queue-using-stacks.py
+++ 0232-implement-queue-using-stacks/0232-implement-queue-using-stacks.py
@@ -1,29 +1,23 @@
 class MyQueue:
 
     def __init__(self):
-        self.queue = []
+        self.s1 = []
+        self.s2 = []
 
     def push(self, x: int) -> None:
-        self.queue.append(x)
+        self.s1.append(x)
 
     def pop(self) -> int:
-        if self.queue:
-            x = self.queue[0]
-            del self.queue[0]
-
-            return x
+        if not self.s2:
+            while self.s1:
+                self.s2.append(self.s1.pop())
+        return self.s2.pop()
 
     def peek(self) -> int:
-        if self.queue:
-            return self.queue[0]
+        if not self.s2:
+            while self.s1:
+                self.s2.append(self.s1.pop())
+        return self.s2[-1]
 
     def empty(self) -> bool:
-        return len(self.queue) == 0
-
-
-# Your MyQueue object will be instantiated and called as such:
-# obj = MyQueue()
-# obj.push(x)
-# param_2 = obj.pop()
-# param_3 = obj.peek()
-# param_4 = obj.empty()
+        return max(len(self.s1), len(self.s2)) == 0
