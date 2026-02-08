e5f93a005edd40c20a60f5be55ec64fb75789eaa
Abhishek Chatterjee <abhishek@imdeepmind.com>
2026-02-08 12:12:11 +0530

    Problem: 0069-sqrtx, Date: 02-08-2026, Space and Time: 19.5 MB (15.26%) 3 ms (63.94%), Language: .py

--- 0069-sqrtx/0069-sqrtx.py
+++ 0069-sqrtx/0069-sqrtx.py
@@ -1,16 +1,15 @@
 class Solution:
     def mySqrt(self, x: int) -> int:
-        start, end = 0, x
+        start, end = 1, x
 
         while end >= start:
-            mid = end - ((end - start) // 2)
+            mid = start + ((end - start) // 2)
 
-            if mid*mid == x:
-                return mid
-            
-            if mid*mid > x:
+            if mid ** 2 > x:
                 end = mid - 1
-            else:
+            elif mid ** 2 < x:
                 start = mid + 1
+            else:
+                return mid
         
         return end
