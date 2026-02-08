ee44d022dd95122cbb1045800c87bc65bd9d6d31
Abhishek Chatterjee <abhishek@imdeepmind.com>
2026-02-08 11:33:44 +0530

    Problem: 0704-binary-search, Date: 02-08-2026, Space and Time: 20.6 MB (45.03%) 0 ms (100%), Language: .py

--- 0704-binary-search/0704-binary-search.py
+++ 0704-binary-search/0704-binary-search.py
@@ -3,14 +3,13 @@ class Solution:
         start, end = 0, len(nums) - 1
 
         while end >= start:
-            mid = end - ((end-start)//2)
-
-            if nums[mid] == target:
-                return mid
+            mid = start + ((end - start) // 2)
 
             if nums[mid] > target:
                 end = mid - 1
-            else:
+            elif nums[mid] < target:
                 start = mid + 1
+            else:
+                return mid
         
         return -1
