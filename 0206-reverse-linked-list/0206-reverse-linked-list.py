97fa2b0008793335bcee1344efc652ac5f05905e
Abhishek Chatterjee <abhishek@imdeepmind.com>
2026-02-08 12:32:30 +0530

    Problem: 0206-reverse-linked-list, Date: 02-08-2026, Space and Time: 20.8 MB (17.53%) 0 ms (100%), Language: .py

--- 0206-reverse-linked-list/0206-reverse-linked-list.py
+++ 0206-reverse-linked-list/0206-reverse-linked-list.py
@@ -5,13 +5,13 @@
 #         self.next = next
 class Solution:
     def reverseList(self, head: Optional[ListNode]) -> Optional[ListNode]:
-        prev, current = None, head
+        curr = head
+        prev = None
 
-        while current:
-            temp = current.next
-            current.next = prev
-
-            prev = current
-            current = temp
+        while curr:
+            temp = curr.next
+            curr.next = prev
+            prev = curr
+            curr = temp
         
         return prev
