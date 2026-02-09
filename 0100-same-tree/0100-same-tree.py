7bf15f516cbaf07c71fe9ba46acc5631e7b61666
Abhishek Chatterjee <abhishek@imdeepmind.com>
2026-02-09 19:34:33 +0530

    Problem: 0100-same-tree, Date: 02-09-2026, Space and Time: 19.4 MB (44.18%) 0 ms (100%), Language: .py

--- 0100-same-tree/0100-same-tree.py
+++ 0100-same-tree/0100-same-tree.py
@@ -6,16 +6,16 @@
 #         self.right = right
 class Solution:
     def isSameTree(self, p: Optional[TreeNode], q: Optional[TreeNode]) -> bool:
-        if not p and q:
-            return False
+        def is_same(p, q):
+            if not p and q:
+                return False
+            
+            if not q and p:
+                return False
+            
+            if not p and not q:
+                return True
+            
+            return p.val == q.val and is_same(p.left, q.left) and is_same(p.right, q.right)
         
-        if p and not q:
-            return False
-        
-        if not p and not q:
-            return True
-        
-        if p.val != q.val:
-            return False
-        
-        return self.isSameTree(p.left, q.left) and self.isSameTree(p.right, q.right)
+        return is_same(p, q)
