6c4fb31fcc31469469d8fe09a55c198f8d590c0c
Abhishek Chatterjee <abhishek@imdeepmind.com>
2026-03-30 19:26:51 +0530

    Problem: 1863-sum-of-all-subset-xor-totals, Date: 03-30-2026, Space and Time: 19.3 MB (51.08%) 11 ms (63.7%), Language: .py

--- 1863-sum-of-all-subset-xor-totals/1863-sum-of-all-subset-xor-totals.py
+++ 1863-sum-of-all-subset-xor-totals/1863-sum-of-all-subset-xor-totals.py
@@ -1,9 +1,9 @@
 class Solution:
     def subsetXORSum(self, nums: List[int]) -> int:
-        def backtrack(i, total=0):
+        def dfs(i, total):
             if i == len(nums):
                 return total
             
-            return backtrack(i+1, total ^ nums[i]) + backtrack(i+1, total)
+            return dfs(i+1, total ^ nums[i]) + dfs(i+1, total)
         
-        return backtrack(0)
+        return dfs(0, 0)
