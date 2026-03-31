f57034c2770754fb9860fc9e697d8ffa798b4893
Abhishek Chatterjee <abhishek@imdeepmind.com>
2026-03-31 19:49:13 +0530

    Problem: 0953-verifying-an-alien-dictionary, Date: 03-31-2026, Space and Time: 19.3 MB (57.47%) 0 ms (100%), Language: .py

--- 0953-verifying-an-alien-dictionary/0953-verifying-an-alien-dictionary.py
+++ 0953-verifying-an-alien-dictionary/0953-verifying-an-alien-dictionary.py
@@ -1,20 +1,17 @@
 class Solution:
     def isAlienSorted(self, words: List[str], order: str) -> bool:
-        nw = len(words)
-        orders = {c: i for i, c in enumerate(order)}
+        mapper = {o:idx for idx, o in enumerate(order)}
 
-        for i in range(nw-1):
+        for i in range(len(words) - 1):
             w1, w2 = words[i], words[i+1]
 
-
-            for j in range(len(w1)):
-                if j >= len(w2):
-                    print(len(w1), len(w2))
+            for idx, c in enumerate(w1):
+                if idx == len(w2):
                     return False
                 
-                if w1[j] != w2[j]:
-                    if orders[w1[j]] > orders[w2[j]]:
+                if c != w2[idx]:
+                    if mapper[c] > mapper[w2[idx]]:
                         return False
                     break
-        
+            
         return True
