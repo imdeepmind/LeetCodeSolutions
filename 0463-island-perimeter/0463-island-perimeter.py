e50da53d1b6694ad9d226bc816209938c5555d97
Abhishek Chatterjee <abhishek@imdeepmind.com>
2026-03-31 19:42:12 +0530

    Problem: 0463-island-perimeter, Date: 03-31-2026, Space and Time: 35.1 MB (11.75%) 110 ms (29.31%), Language: .py

--- 0463-island-perimeter/0463-island-perimeter.py
+++ 0463-island-perimeter/0463-island-perimeter.py
@@ -1,24 +1,20 @@
 class Solution:
     def islandPerimeter(self, grid: List[List[int]]) -> int:
-        visits = set()
-        def dfs(i, j):
-            if i < 0 or j < 0:
-                return 1
+        rows, cols = len(grid), len(grid[0])
+        visit = set()
 
-            if i >= len(grid) or j >= len(grid[0]):
+        def dfs(i=0, j=0):
+            if i < 0 or j < 0 or i >= rows or j >= cols or grid[i][j] == 0:
                 return 1
-
-            if grid[i][j] == 0:
-                return 1
-            
-            if (i,j) in visits:
+            elif (i ,j) in visit:
                 return 0
             
-            visits.add((i,j))
-            
-            return dfs(i+1, j) + dfs(i, j+1) + dfs(i-1, j) + dfs(i, j-1)
-
+            visit.add((i, j))
+            return dfs(i-1, j) + dfs(i, j-1) + dfs(i+1, j) + dfs(i, j+1)
+        
         for i in range(len(grid)):
             for j in range(len(grid[i])):
                 if grid[i][j]:
                     return dfs(i, j)
+
+        return 0
