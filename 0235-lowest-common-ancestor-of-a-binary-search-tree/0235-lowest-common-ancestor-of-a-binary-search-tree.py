db6efd42a9fe8a8a4b6209a9eecfc3409d0c001a
Abhishek Chatterjee <abhishek@imdeepmind.com>
2026-06-26 19:37:41 +0530

    Problem: 0235-lowest-common-ancestor-of-a-binary-search-tree, Date: 06-26-2026, Space and Time: 22.7 MB (43.71%) 63 ms (69.42%), Language: .py

--- 0235-lowest-common-ancestor-of-a-binary-search-tree/0235-lowest-common-ancestor-of-a-binary-search-tree.py
+++ 0235-lowest-common-ancestor-of-a-binary-search-tree/0235-lowest-common-ancestor-of-a-binary-search-tree.py
@@ -1,16 +1,19 @@
 # Definition for a binary tree node.
 # class TreeNode:
-#     def __init__(self, x):
-#         self.val = x
-#         self.left = None
-#         self.right = None
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
 
 class Solution:
-    def lowestCommonAncestor(self, root: 'TreeNode', p: 'TreeNode', q: 'TreeNode') -> 'TreeNode':
-        while root:
-            if root.val < min(q.val, p.val):
-                root = root.right
-            elif root.val > max(p.val, q.val):
-                root = root.left
-            else:
-                return root
+    def lowestCommonAncestor(self, root: TreeNode, p: TreeNode, q: TreeNode) -> TreeNode:
+        if not root or not q or not q:
+            return None
+        
+        if max(p.val, q.val) < root.val:
+            return self.lowestCommonAncestor(root.left, p, q)
+        
+        if min(p.val, q.val) > root.val:
+            return self.lowestCommonAncestor(root.right, p, q)
+        
+        return root
