db1becb3dcf8b3afced717d3b46a39a72351f697
Abhishek Chatterjee <abhishek@imdeepmind.com>
2026-06-26 19:11:56 +0530

    Problem: 0110-balanced-binary-tree, Date: 06-26-2026, Space and Time: 20.5 MB (39.21%) 3 ms (52.8%), Language: .py

--- 0110-balanced-binary-tree/0110-balanced-binary-tree.py
+++ 0110-balanced-binary-tree/0110-balanced-binary-tree.py
@@ -4,17 +4,18 @@
 #         self.val = val
 #         self.left = left
 #         self.right = right
+
 class Solution:
     def isBalanced(self, root: Optional[TreeNode]) -> bool:
         def dfs(head):
             if not head:
-                return True, 0
+                return (True, 0)
             
             left_balanced, left_height = dfs(head.left)
             right_balanced, right_height = dfs(head.right)
 
             return (
-                left_balanced and right_balanced and abs(left_height - right_height) <= 1, 
+                left_balanced and right_balanced and abs(left_height - right_height) <= 1,
                 1 + max(left_height, right_height)
             )
         
