9cabca62cc3cf0e5e482abfbd365692c41383c24
Abhishek Chatterjee <abhishek@imdeepmind.com>
2026-06-26 18:59:15 +0530

    Problem: 0543-diameter-of-binary-tree, Date: 06-26-2026, Space and Time: 22.2 MB (65.04%) 7 ms (23.87%), Language: .py

--- 0543-diameter-of-binary-tree/0543-diameter-of-binary-tree.py
+++ 0543-diameter-of-binary-tree/0543-diameter-of-binary-tree.py
@@ -4,23 +4,24 @@
 #         self.val = val
 #         self.left = left
 #         self.right = right
+
 class Solution:
     def diameterOfBinaryTree(self, root: Optional[TreeNode]) -> int:
         res = 0
 
-        def dfs(head):
+        def calculate(head):
             nonlocal res
 
             if not head:
                 return 0
             
-            left_height = dfs(head.left)
-            right_height = dfs(head.right)
+            left_diameter = calculate(head.left)
+            right_diameter = calculate(head.right)
 
-            res = max(res, left_height + right_height)
+            res = max(res, left_diameter + right_diameter)
 
-            return 1 + max(left_height, right_height)
+            return 1 + max(left_diameter, right_diameter)
         
-        dfs(root)
+        calculate(root)
 
         return res
